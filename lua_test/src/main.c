d97bb33e63a067b72499af2cb64c9c7a933e388a
Patrick <Patrick_DJ@163.com>
2012-08-04 08:41:01 +0800

    .

--- lua_test/src/main.c
+++ lua_test/src/main.c
@@ -5,7 +5,37 @@
 
 int main()
 {
-    lua_State *L = lua_open();
+    const char *file;
+    lua_State *L;
+    
+    file = "config.lua";
+    L = lua_open();
+    luaL_openlibs(L);
+
+    if (luaL_loadfile(L, file) || lua_pcall(L, 0, 0, 0))
+    {
+        printf("%s\n", lua_tostring(L, -1));
+    }
+
+    printf("top: %d\n", lua_gettop(L));
+
+    lua_getglobal(L, "color");
+    printf("%d\n", lua_istable(L, -1));
+    printf("%d\n", lua_type(L, -1));
+
+    printf("top: %d\n", lua_gettop(L));
+    lua_pushstring(L, "r");
+    printf("top: %d\n", lua_gettop(L));
+    lua_gettable(L, -2);
+
+    printf("top: %d\n", lua_gettop(L));
+    printf("%d\n", lua_isnumber(L, -1));
+    printf("top: %d\n", lua_gettop(L));
+    printf("%d\n", (int)lua_tonumber(L, -1));
+    printf("%d\n", (int)lua_tonumber(L, -1));
+
+    lua_pop(L, 1);
+    printf("top: %d\n", lua_gettop(L));
 
     return 0;
 }
